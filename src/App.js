0b5dda99cae5fbecf996c162bb2fb845dd8f09ea
Fatuma Meshalla <mompround@gmail.com>
2023-04-12 14:49:15 -0700

    final commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+import './App.css';
+import UserFrom from './components/UserFrom';
+
+const App =()=> {
+  return (
+    <div className="App">
+      
+     < UserFrom/>
+      
+    </div>
+  );
+}
+
+export default App;
